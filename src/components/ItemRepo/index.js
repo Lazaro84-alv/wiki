42878ee2a84163c35a3c65043e5a83b6001e46c3
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-06 17:47:02 -0300

    Implementa botoes

--- src/components/ItemRepo/index.js
+++ src/components/ItemRepo/index.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { ItemContainer } from './styles';
 
 
-function ItemRepo() {
+function ItemRepo({repo, handleRemoveRepo}) {
+
+  const handleRemove = () => {
+    handleRemoveRepo(repo.id)
+  }
+
   return (
-    <ItemContainer>
-      <h3>Lázaro</h3>
-      <p>gitfind/lazaro</p>
-      <a href="#">Ver repositório</a><br />
-      <a href="#" className="remover">Remover</a>
+    <ItemContainer onClick={handleRemove}>
+      <h3>{repo.name}</h3>
+      <p>{repo.full_name}</p>
+      <a href={repo.html_url} target="_blank" rel="noreferrer">Ver repositório</a><br />
+      <a href="#" rel="noreferrer" className="remover">Remover</a>
       <hr />
     </ItemContainer>
   )
