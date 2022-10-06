cf678d45f2f3ec9577d45a43e687e569f9c8b8d9
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-06 14:24:43 -0300

    Add input

--- src/pages/styles.js
+++ src/pages/styles.js
@@ -3,10 +3,9 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   height: 90vh;
-
   margin: 20px 0 0 0;
-
   display: flex;
-  align-Items: flex-start;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
 `
